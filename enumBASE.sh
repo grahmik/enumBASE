982c96b97ae490e85c0727d6d38bb28f5a97fe12
Mike Graham <125515783+grahmik@users.noreply.github.com>
2023-08-24 18:22:49 -0500

    Update enumBASE.sh

--- enumBASE.sh
+++ enumBASE.sh
@@ -48,6 +48,7 @@ echo -e " $green----------------------------------------------------------------
  sleep .2
  echo -e "$green|$reset by: Mike Graham @grahmik                                           $green|"
  echo -e " --------------------------------------------------------------------$reset"
+ sleep 1
 
 # Remove the enum directory if it exists #####################
 rm -r .enum 2>/dev/null
